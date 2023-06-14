7187e0ffb016c4eac1c1a2a6ed6aaa5446887105
Natallia Varabyova <natalija1111111@gmail.com>
2023-06-14 22:00:34 +0300

    changed style queries

--- queries.sql
+++ queries.sql
@@ -21,50 +21,51 @@ SELECT
  ORDER BY income DESC NULLS LAST
  LIMIT 10;
 
--- Step 5.2. Looking for the sellers with average income that is less then total average income.
--- For that we need to join the table 'employees' with the table 'sales' and the table sales with
--- the table 'products'
-select
-       concat(e.first_name, ' ', e.last_name) as name,
+/* Step 5.2. Looking for the sellers with average income that is less then total average income.
+For that we need to join the table 'employees' with the table 'sales' and the table sales with
+the table 'products'*/
+SELECT
+       CONCAT(e.first_name, ' ', e.last_name) AS name,
        -- combining first_name and last_name into one line
-       round(avg(s.quantity * p.price), 0) as average_income
+       ROUND(AVG(s.quantity * p.price), 0) AS average_income
        -- looking for the average income for every single seller
-  from employees e
-  left join sales s
-    on e.employee_id = s.sales_person_id
-  left join products p
-    on s.product_id = p.product_id
- group by concat(e.first_name, ' ', e.last_name)
-having round(avg(s.quantity * p.price), 0) < (select
-       round(avg(s.quantity * p.price), 0)
-       from sales s
-       -- comparing the average income of each seller with the total average income
-       left join products p
-       on s.product_id = p.product_id)
- order by average_income;
+  FROM employees e
+  LEFT JOIN sales s
+       ON e.employee_id = s.sales_person_id
+  LEFT JOIN products p
+       ON s.product_id = p.product_id
+ GROUP BY CONCAT(e.first_name, ' ', e.last_name)
+HAVING ROUND(AVG(s.quantity * p.price), 0) <
+       (SELECT
+               ROUND(AVG(s.quantity * p.price), 0)
+          FROM sales s
+         -- comparing the average income of each seller with the total average income
+          LEFT JOIN products p
+               ON s.product_id = p.product_id)
+ ORDER BY average_income;
 
--- Step 5.3. Looking for total income for every seller and every day of week. For that we need
--- to join the table 'employees' with the table 'sales' and the table sales with the table 'products'
- select
-      concat(e.first_name, ' ', e.last_name) as name,
-      -- combine first_name and last_name into one line
-      to_char(s.sale_date, 'day') as weekday,
-      -- finding out a day of week
-      round(sum(s.quantity * p.price), 0) as income
-      -- looking for the total income for every seller
- from employees e
- left join sales s
-   on e.employee_id = s.sales_person_id
- left join products p
-   on s.product_id = p.product_id
-group by
-      to_char(s.sale_date, 'day'),
-      concat(e.first_name, ' ', e.last_name),
-      extract(isodow from s.sale_date)
-      -- looking for the weekday number where the day of the week as Monday (1) to Sunday (7)
-order by
-      extract(isodow from s.sale_date),
-      name;
+/* Step 5.3. Looking for total income for every seller and every day of week. For that we need
+to join the table 'employees' with the table 'sales' and the table sales with the table 'products' */
+SELECT
+       CONCAT(e.first_name, ' ', e.last_name) AS name,
+       -- combine first_name and last_name into one line
+       TO_CHAR(s.sale_date, 'day') AS weekday,
+       -- finding out a day of week
+       ROUND(SUM(s.quantity * p.price), 0) AS income
+       -- looking for the total income for every seller
+  FROM employees e
+  LEFT JOIN sales s
+       ON e.employee_id = s.sales_person_id
+  LEFT JOIN products p
+       ON s.product_id = p.product_id
+ GROUP BY
+       TO_CHAR(s.sale_date, 'day'),
+       CONCAT(e.first_name, ' ', e.last_name),
+       EXTRACT(ISODOW FROM s.sale_date)
+       -- looking for the weekday number where the day of the week as Monday (1) to Sunday (7)
+ ORDER BY
+       EXTRACT(ISODOW FROM s.sale_date),
+       name;
 
 /* Step 6.1. Looking for the number of customers of different age groups.*/
 SELECT
